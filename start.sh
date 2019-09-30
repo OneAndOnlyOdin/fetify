65fd7c86d5a1efdc2c8b1f606693bef7532f8d35
Sceiuck <leikho@gmail.com>
2019-09-30 21:38:30 +0800

    Fix start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-#/bin/sh
-docker run -dt -p 3040:3000 --env-file .env --name fetify --restart=always fetify:master
+#!/bin/sh
+docker run -dt -p 3040:3000 --env-file .env --name fetify --restart=always --network sh fetify:master
