bdb086d5e55a159e062988086b13de44605d6050
Sceiuck <leikho@gmail.com>
2019-12-31 20:29:52 +0800

    Create unique indexes for state and read models

--- src/db/indexes.ts
+++ src/db/indexes.ts
@@ -1,7 +1,11 @@
 import { Db } from 'mongodb'
 
 export async function ensureIndexes(db: Db) {
-  await db
-    .collection('auth')
-    .createIndex({ username: 1 }, { name: 'auth_username', unique: true })
+  await db.collection('auth').createIndex({ username: 1 }, { name: 'auth_username', unique: true })
+
+  await db.collection('userProfiles').createIndex({ userId: 1 }, { name: 'userProfiles_userId', unique: true })
+
+  await db.collection('gameLock').createIndex({ id: 1 }, { name: 'gameLock_id', unique: true })
+
+  await db.collection('lockState').createIndex({ aggregateId: 1 }, { name: 'lockState_aggregateId', unique: true })
 }
