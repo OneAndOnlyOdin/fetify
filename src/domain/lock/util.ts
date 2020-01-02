6df55fac5010e0f4c13bdaec99d75176d5792736
Sceiuck <leikho@gmail.com>
2020-01-02 17:09:11 +0800

    Fix reset deduction

--- src/domain/lock/util.ts
+++ src/domain/lock/util.ts
@@ -120,7 +120,7 @@ function applyAction(action: LockAction, actions: LockAction[], cfg: LockConfig)
     }
 
     case 'reset': {
-      const resetsLeft = actions.filter(act => act.type === 'reset').length
+      const resetsLeft = cfg.actions.reset - actions.filter(act => act.type === 'reset').length
       const resets = createActions(resetsLeft, 'reset')
       const nextActions = createConfigActions({
         ...cfg.actions,
